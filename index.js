4f29f9f8d6e9271525958b8b05a809e3bbeeb367
Rick <rick329mo@gmail.com>
2022-07-08 15:57:47 -0500

    Added a save feature to the JS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
-    console.log(count)
+    let countStr = count + "/"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
+
